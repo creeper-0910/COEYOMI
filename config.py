cb73f2fd19321359adf164850bc89120f4b8294f
creeper-0910 <56744841+creeper-0910@users.noreply.github.com>
2024-03-02 22:23:37 +0900

    修正

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+token = os.environ['token']
 url_regex = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 
 default_uuid = "3c37646f-3881-5374-2a83-149267990abc"
